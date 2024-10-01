bf01cc593e8cc5fa36ebae6db8ba17657d432d61
Yuliia Diachenko <zirkanew82@gmail.com>
2024-10-01 14:57:39 +0300

    Reviews add fetch

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCamper } from '../../redux/campers/operations';
+import { selectCamper } from '../../redux/campers/selectors';
 import css from './Reviews.module.css';
 import BookForm from '../BookForm/BookForm.jsx';
 import icons from '../../image/symbol-defs.svg';
 
 export default function Reviews() {
+
+    const dispatch = useDispatch();
+    const camper = useSelector(selectCamper);
+  console.log(camper);
+    const {id} = useParams();
+
+    useEffect(() => {
+        
+          dispatch(fetchCamper(id)); 
+
+      }, [ dispatch, id ]);
 
     return (
         <div className={css.container}>
@@ -13,31 +29,25 @@ export default function Reviews() {
                     <div className={css.avatarContainer}>
                     <span className={css.avatarCircle}>A</span>
                     <div className={css.nameContainer}>
-                        <h6 className={css.name}>Alice</h6>
+                        <h6 className={css.name}>{camper.reviews[0]?.reviewer_name}</h6>
                         <svg width="96" height="30">
                             <use href={`${icons}#icon-rate-five`}></use>
                         </svg>
                     </div>   
                         </div>
-                        <p className={css.text}>The Mavericks panel truck was a perfect choice for my solo road trip.
-                             Compact, easy to drive, and had all the essentials. 
-                             The kitchen facilities were sufficient, and the overall 
-                             experience was fantastic.</p>
+                        <p className={css.text}>{camper.reviews[0]?.comment}</p>
                     </li>
                     <li>
                     <div className={css.avatarContainer}>
                     <span className={css.avatarCircle}>B</span>
                     <div className={css.nameContainer}>
-                        <h6 className={css.name}>Bob</h6>
+                        <h6 className={css.name}>{camper.reviews[1]?.reviewer_name}</h6>
                         <svg width="96" height="30">
                             <use href={`${icons}#icon-rate-three`}></use>
                         </svg>
                     </div>
                     </div>
-                        <p className={css.text}>The Mavericks panel truck was a perfect choice for my solo road trip.
-                             Compact, easy to drive, and had all the essentials. 
-                             The kitchen facilities were sufficient, and the overall 
-                             experience was fantastic.</p>
+                        <p className={css.text}>{camper.reviews[1]?.comment}</p>
 
                     </li>
                 </ul>
